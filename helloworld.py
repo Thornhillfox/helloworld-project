1508713e50e25133e6f55fb614eaf1b2722f167f
Fox <thornhillfox@gmail.com>
2020-11-05 21:53:36 +0300

    simple text

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+print('Hello world')
+
+# echo "# helloworld-project" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/Thornhillfox/helloworld-project.git
+# git push -u origin main
